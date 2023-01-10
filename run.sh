e8662312b4686410bbfa0ff8cec5ee313c1d679a
kursadalsan <108494826+kursadalsan@users.noreply.github.com>
2023-01-10 23:43:56 +0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+mkdir $1 
+cd $1
+amass intel -whois -d $1 > 1.txt
+amass enum -passive -df 1.txt > 2.txt
+subfinder -dL 1.txt -o 3.txt
+cat *.txt > 4.txt
+httpx -l 4.txt -o 5.txt -t 150
+nuclei -rl 550 -bs 100 -s low -s medium -s high -s critical -l 5.txt 
